681109dc9b34d24d23ea925d04adaae1f6284315
Sr-RopBrian <kipngenobrian070@gmail.com>
2025-12-01 18:23:55 +0300

    Initial commit of the Cantus app.

---
+++ app/src/main/java/com/example/cantus/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.cantus
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.cantus.ui.theme.CantusTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CantusTheme {
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { paddingValues ->
+                    CantusApp(
+                        modifier =  Modifier.padding(paddingValues)
+                    )
+                }
+            }
+        }
+    }
+}
